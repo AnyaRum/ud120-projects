c82435e0d0abed6997d133e70b6f00d8adb17ac5
Anna Rumyantseva <Rumyantseva.anya@gmail.com>
2016-04-25 21:06:05 +0100

    Finished the mini-project on evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,27 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn import tree
+from sklearn import cross_validation
+import numpy as np
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+pred = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, pred)
+print(acc)	
+print "Number of identified POIs"
+print(np.sum(pred))
+print "Total number of people in test set"
+print(len(pred))
+
+
+from sklearn.metrics import precision_score
+print(precision_score(y_test, pred))
+
+from sklearn.metrics import recall_score
+print(recall_score(y_test, pred))
