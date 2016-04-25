e098427a38e77390b4f9211cca1ad13011080f03
Anna Rumyantseva <Rumyantseva.anya@gmail.com>
2016-04-25 20:40:13 +0100

    Finished the mini-project on validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -29,4 +29,15 @@ labels, features = targetFeatureSplit(data)
 
 ### it's all yours from here forward!  
 
+from sklearn import tree
+from sklearn import cross_validation
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+
+pred = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, pred)
+print(acc)	
